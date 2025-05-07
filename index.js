3ce3af0db3c91cf67be20d4a63bacf246101c29a
TarunMulchandani <tarunmulchandaniwork204@gmail.com>
2025-05-07 21:48:36 +0530

    cleaned

--- index.js
+++
@@ -1,4 +0,0 @@
-console.log("be Kind");
-alert("danger ahead");
-document.getElementById("Heading").textContent = "Welcome to this World";
-document.getElementById("para").textContent = "You Gonna regret it soon";
